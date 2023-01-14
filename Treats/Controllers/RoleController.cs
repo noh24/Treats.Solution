1122fd8a9cf8c2efb578f3b09c9d22115c902cf3
Brian <noh24@ymail.com>
2023-01-14 15:45:37 -0500

    display users in roles

--- Treats/Controllers/RoleController.cs
+++ Treats/Controllers/RoleController.cs
@@ -21,9 +21,16 @@ namespace Treats.Controllers
       _userManager = userManager;
     }
 
-    public ViewResult Index()
+    public async Task<ViewResult> Index()
     {
-      return View(_roleManager);
+      var roles = _roleManager.Roles;
+        var usersInRoles = new Dictionary<IdentityRole, IList<ApplicationUser>>();
+        foreach (var role in roles.ToList())
+        {
+            var users = await _userManager.GetUsersInRoleAsync(role.Name);
+            usersInRoles.Add(role, users);
+        }
+      return View(usersInRoles);
     }
     private void Errors(IdentityResult result)
     {
